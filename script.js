23af6042f70f13e9455bf7c08f0747f714d34325
Simon <sema92m@wp.pl>
2022-10-11 20:16:59 +0200

    Add API key and some styles

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+const url = `https://api.thecatapi.com/v1/images/search`;
+const api_key = "live_iWJfMBk1zt3SIyXoj3Y5Cz8p7jA1OIYELEZvF0Zjy4vM47mlhtlF4sMqefULDGC3"
+
 let generate_btn = document.querySelector(".generate_btn");
 generate_btn.addEventListener("click", fetchPics);
 
@@ -5,8 +9,9 @@ function fetchPics() {
     let catsImgDiv = document.querySelector(".catsImgDiv");
     catsImgDiv.innerHTML = "";
 
-
-    fetch("https://api.thecatapi.com/v1/images/search")
+    fetch(url,{headers: {
+		'x-api-key': api_key
+	  }})
         .then((response) => response.json())
         .then((data) => {
             let catsImgUrl = data[0].url;
@@ -15,8 +20,7 @@ function fetchPics() {
             catsImgEl.setAttribute("src", `${catsImgUrl}`);
             catsImgEl.classList.add("showcase");
 
-
-			let catsImgDiv = document.querySelector(".catsImgDiv");
+            let catsImgDiv = document.querySelector(".catsImgDiv");
             catsImgDiv.appendChild(catsImgEl);
         })
         .catch((err) => console.log(err));
